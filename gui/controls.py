21b5c967c178a35facc0d2cdb070b7bf92fe28fe
root <root@M16>
2025-04-29 00:48:27 -0300

    contraste funcionando

--- gui/controls.py
+++ gui/controls.py
@@ -5,16 +5,16 @@ class Controls:
         self.main_window = main_window
         self.frame = tk.Frame(root)
         
-        self.sobel_button = tk.Button(self.frame, text="Apply Sobel", command=self.main_window.add_sobel_filter)
+        self.sobel_button = tk.Button(self.frame, text="Apply Sobel", command=self.main_window.switch_sobel_filter)
         self.sobel_button.pack(side=tk.LEFT, padx=5)
         
-        self.median_button = tk.Button(self.frame, text="Apply Median", command=self.main_window.add_median_filter)
+        self.median_button = tk.Button(self.frame, text="Apply Median", command=self.main_window.switch_median_filter)
         self.median_button.pack(side=tk.LEFT, padx=5)
         
-        self.invert_button = tk.Button(self.frame, text="Invert Colors", command=self.main_window.add_invert_colors)
+        self.invert_button = tk.Button(self.frame, text="Invert Colors", command=self.main_window.switch_invert_colors)
         self.invert_button.pack(side=tk.LEFT, padx=5)
         
-        self.contrast_scale = tk.Scale(self.frame, from_=0.5, to=2.0, resolution=0.1, orient=tk.HORIZONTAL, label="Contrast", command=self.main_window.add_adjust_contrast)
+        self.contrast_scale = tk.Scale(self.frame, from_=0.5, to=2.0, resolution=0.1, orient=tk.HORIZONTAL, label="Contrast", command=self.main_window.switch_adjust_contrast)
         self.contrast_scale.set(1.0)
         self.contrast_scale.pack(side=tk.LEFT, padx=5)
         
--- gui/main_window.py
+++ gui/main_window.py
@@ -11,7 +11,6 @@ class MainWindow:
         self.file_manager = FileManager()
         self.original_images = self.file_manager.load_images()
         self.current_image = self.original_images[0].copy()
-        self.filter_applied = False
         
         self.image_viewer = ImageViewer(
             self.root, 
@@ -25,7 +24,12 @@ class MainWindow:
         self.controls = Controls(self.root, self)
         self.controls.frame.pack(side=tk.BOTTOM, padx=10, pady=10)
         self.current_image_index = 0
-        self.filters = {}
+        self.filters = {
+            'sobel': False,
+            'median': False,
+            'invert': False,
+            'contrast': 1.0
+        }
     
     def change_active_image(self, image_index):
         self.current_image_index = image_index
@@ -33,60 +37,61 @@ class MainWindow:
         self.image_viewer.update_images(self.original_images[image_index], self.current_image)
         self.controls.contrast_scale.set(1.0)
     
-    def add_sobel_filter(self):
-        if 'sobel' in self.filters:
-            self.filters['sobel'] = False
-        else: 
-            self.filters['sobel'] = True
+    def switch_sobel_filter(self):
+        self.filters['sobel'] = not self.filters['sobel']
+        self.apply_filters()
 
-    def add_median_filter(self):
-        if 'median' in self.filters:
-            self.filters['median'] = False
-        else: 
-            self.filters['median'] = True
+    def switch_median_filter(self):
+        self.filters['median'] = not self.filters['median']
+        self.apply_filters()
 
-    def add_invert_colors(self):
-        if 'invert' in self.filters:
-            self.filters['invert'] = False
-        else: 
-            self.filters['invert'] = True
+    def switch_invert_colors(self):
+        self.filters['invert'] = not self.filters['invert']
+        self.apply_filters()
 
-    def add_adjust_contrast(self, value):
+    def switch_adjust_contrast(self, value):
         self.filters['contrast'] = value
+        self.apply_filters()
+
+    def apply_filters(self):
+        self.current_image = self.original_images[self.current_image_index].copy()
+        if self.filters.get('sobel'):
+            self.apply_sobel_filter()
+        if self.filters.get('median'):
+            self.apply_median_filter()
+        if self.filters.get('invert'):
+            self.invert_colors()
+        if 'contrast' in self.filters:
+            self.adjust_contrast(self.filters['contrast'])
+        self.image_viewer.update_modified_image(self.current_image)
 
     def apply_sobel_filter(self):
         from core.image_processor import ImageProcessor
         self.current_image = ImageProcessor.apply_sobel(self.current_image)
-        self.image_viewer.update_modified_image(self.current_image)
-        self.filter_applied = True
+        
     
     def apply_median_filter(self):
         from core.image_processor import ImageProcessor
         self.current_image = ImageProcessor.apply_median(self.current_image)
-        self.image_viewer.update_modified_image(self.current_image)
-        self.filter_applied = True
     
     def invert_colors(self):
         from core.image_processor import ImageProcessor
         self.current_image = ImageProcessor.invert_colors(self.current_image)
-        self.image_viewer.update_modified_image(self.current_image)
-        self.filter_applied = True
     
     def adjust_contrast(self, value):
         from core.image_processor import ImageProcessor
-        base_image = self.original_images[self.current_image_index].copy()
-        if (self.filter_applied):
-            base_image = self.current_image.copy()
-            base_image = ImageProcessor.adjust_contrast(base_image, float(value))
-            self.image_viewer.update_modified_image(base_image)
-        else:
-            self.current_image = ImageProcessor.adjust_contrast(base_image, float(value))
-            self.image_viewer.update_modified_image(self.current_image)
+        self.current_image = ImageProcessor.adjust_contrast(self.current_image, float(value))
 
     def save_modified_image(self):
         self.file_manager.save_image(self.current_image)
 
     def reset_image(self):
         self.current_image = self.original_images[self.current_image_index].copy()
-        self.image_viewer.update_modified_image(self.current_image)
         self.controls.contrast_scale.set(1.0)
+        self.image_viewer.update_modified_image(self.current_image)
+        self.filters = {
+            'sobel': False,
+            'median': False,
+            'invert': False,
+            'contrast': 1.0
+        }
